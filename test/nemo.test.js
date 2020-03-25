cfda52493c56d71463cb403c1417b72814e6a530
Eric A <rinat0025@gmail.com>
2020-03-24 22:58:05 -0400

    added reports to ignore file + getting driver title with assertion

--- test/nemo.test.js
+++ test/nemo.test.js
@@ -1,3 +1,4 @@
+const assert = require('assert');
 describe('@firstTest@', function () {
   // before(async function () {
   //   // // Chroem driver issues ? follow this steps : https://tecadmin.net/setup-selenium-chromedriver-on-ubuntu/
@@ -18,19 +19,19 @@ describe('@firstTest@', function () {
   //   // await login.form.waitVisible(3000);
   // });
   
-  it('should be able to reach Jquire website', async function () {
+  it('should be able to reach jQuery UI website', async function () {
     // // Chroem driver issues ? follow this steps : https://tecadmin.net/setup-selenium-chromedriver-on-ubuntu/
     let nemo = this.nemo;
     await nemo.driver.get(nemo.data.baseUrl);
-    // ID locator
-    let title = await nemo.driver.get(nemo.document.title);
-    console.log('page title is : ' + title);
-
+    let actualTitle = await nemo.driver.getTitle();
+      console.log('Current page title is', actualTitle);
+      
     // let sideBar = await nemo.view._finds('#sidebar');
     // console.log("List of avaliable sidebars : " + sideBar);
     await nemo.snap();
-    await nemo.driver.sleep(2000);
-    await this.nemo.driver.quit();
+    // await nemo.driver.sleep(200);
+    await nemo.driver.close();
+    // await this.nemo.driver.quit();
   });
 
   // xit('should load a website', async function () {
