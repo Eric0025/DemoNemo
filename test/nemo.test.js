ea9b824d307e156408efa06464c68a4dcb5015f8
Eric A <rinat0025@gmail.com>
2020-04-04 21:19:30 -0400

    Added tests for Interactions

--- test/nemo.test.js
+++ test/nemo.test.js
@@ -1,45 +1,117 @@
 const assert = require('assert');
+const { inspect } = require('util');
+let nemo
+
 describe('@firstTest@', function () {
   beforeEach(async function () {
     // Chroem driver issues ? follow this steps : https://tecadmin.net/setup-selenium-chromedriver-on-ubuntu/
-    let nemo = this.nemo;
+    nemo = this.nemo;
     await nemo.driver.get(nemo.data.baseUrl);
     await nemo.driver.manage().setTimeouts({ implicit: 3000, pageLoad: 3000, script: 3000 });
     await nemo.driver.manage().window().maximize();
   });
+
   after(async function () {
-    let nemo = this.nemo;
+    nemo = this.nemo;
     await nemo.driver.close();
   });
   
   xit('should be able to reach jQuery UI website', async function () {
     // // Chroem driver issues ? follow this steps : https://tecadmin.net/setup-selenium-chromedriver-on-ubuntu/
-    let nemo = this.nemo;
+    nemo = this.nemo;
     await nemo.driver.getTitle().then(function (title) {
       // console.log('The page title is ' + title);
       assert.equal(title, 'jQuery UI');
     });
-      
-    // let sideBar = await nemo.view._finds('#sidebar');
-    // console.log("List of avaliable sidebars : " + sideBar);
     
-    // await nemo.driver.sleep(200);
-    // await nemo.snap();
-    // await nemo.driver.close();
-    // await this.nemo.driver.quit();
   });
 
-  it('should load a website', async function () {
-    let nemo = this.nemo;
-    // await nemo.view._waitVisible('link:Draggable');
-    let draggableElem = await nemo.view._find('aside.widget:nth-child(1) > ul:nth-child(2) > li:nth-child(1) > a:nth-child(1)').click();
-    // let draggableWait = await nemo.view._waitVisible('#draggable', 5000, 'webelement not able to locate');
-    await nemo.view._find('.demo-frame').then(function(frame){
+  xit('should drage and drop', async function () {
+    nemo = this.nemo;
+    await nemo.view._find('xpath://a[contains(text(),"Draggable")]').click();
+    await nemo.view._find('css:.demo-frame').then(function(frame){
       return nemo.driver.switchTo().frame(frame);
   });
-    // temp = await draggableWait.nemo.view_find('#draggable');
-    // console.log('swithced to frame', iFrameSwitch.getText());
+    let draggableWB = await nemo.view._find('id:draggable');
+    await nemo.driver.actions().dragAndDrop(draggableWB, { x: 100, y: 100 }).perform();
   }); 
+
+  xit('should drop to the location', async function () {
+    nemo = this.nemo;
+    await nemo.view._find('xpath://a[contains(text(),"Droppable")]').click();
+    await nemo.view._find('css:.demo-frame').then(function(frame){
+      return nemo.driver.switchTo().frame(frame);
+  });
+    let draggableWB = await nemo.view._find('id:draggable');
+    let droppableWB = await nemo.view._find('id:droppable');
+    await nemo.driver.actions().dragAndDrop(draggableWB, droppableWB).perform();
+  }); 
+
+  xit('should resize the resizeable element', async function () {
+    nemo = this.nemo;
+    await nemo.view._find('xpath://a[contains(text(),"Resizable")]').click();
+    await nemo.view._find('css:.demo-frame').then(function(frame){
+      return nemo.driver.switchTo().frame(frame);
+  });
+    let resizableWB = await nemo.view._find('xpath://*[@id="resizable"]/div[3]');
+    await nemo.driver.actions().dragAndDrop(resizableWB, { x: 100, y: 250 }).perform();
+  }); 
+
+  it('should select the selectable iteams', async function () {
+    nemo = this.nemo;
+    await nemo.view._find('xpath://a[contains(text(),"Selectable")]').click();
+    await nemo.view._find('css:.demo-frame').then(function(frame){
+      return nemo.driver.switchTo().frame(frame);
+  });
+  
+    // let selectableWB = await nemo.view._finds('xpath:/html/body/ol');
+    // let temp = selectableWB.getSize();
+    // console.log(temp)
+    
+  //   for (let i = 0; i < 7; i++) {
+  //     await nemo.view._finds('xpath:/html/body/ol/li['+i+']');
+  //     await nemo.driver.actions().click(selectableWB).perform();
+  // }
+
+  // let listArray = new listArray();
+  // let element1 = await nemo.view._find('xpath:/html/body/ol');
+  
+  
+    await nemo.view._finds('#selectable').then(function(elements){      
+      elements.forEach(function (el) {
+        let size = el.getSize();
+        console.log('******************'+size.toString);
+        console.log('******************'+inspect(el));
+        el.click();
+        nemo.driver.sleep(2500);
+      })
+    });
+
+
+
+      // nemo.view._find('xpath:/html/body/ol/li['+i+']');
+      // nemo.driver.actions().click(i).perform();
+
+    
+
+    // array.forEach(element => {
+    //   nemo.driver.actions().click(selectableWB).perform();
+    // });
+    
+    // await nemo.driver.actions().click(selectableWB).perform();
+  }); 
+
+  xit('should sort the sortable iteams', async function () {
+    nemo = this.nemo;
+    await nemo.view._find('xpath://a[contains(text(),"Sortable")]').click();
+    await nemo.view._find('css:.demo-frame').then(function(frame){
+      return nemo.driver.switchTo().frame(frame);
+  });
+    let sortableWB = await nemo.view._find('xpath://li[@class="ui-state-default ui-sortable-handle"]');
+    await nemo.driver.actions().click(sortableWB).dragAndDrop(sortableWB, { x: 7, y: 100 }).perform();
+    await nemo.driver.sleep(4000);
+  }); 
+  
 
 });
 
@@ -51,4 +123,9 @@ describe('@firstTest@', function () {
   // await nemo.snap();     
   // await nemo.view._find('.FPdoLc > center:nth-child(1) > input:nth-child(1)').click();     
   // await nemo.driver.sleep(4000);
+
+  // await nemo.view._waitVisible('id:draggable', 5000, 'webelement not able to locate').getText().then(function(txt){
+  //   console.log('draggable element name is: ', txt);
+  //   assert.equal(txt, 'Drag me around', 'expected element name not found Please check for the log-AssertionError');
+  // });
   
